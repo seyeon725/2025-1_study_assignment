7ee8174f5b35e64a415503c6c43695018a1c1a83
seyeon725 <seyeon0725@naver.com>
2025-04-09 15:08:44 +0900

    hagisilta

--- assignment4/4_2_star_ans.cs
+++ assignment4/4_2_star_ans.cs
@@ -10,9 +10,65 @@ namespace star
             int radius = int.Parse(Console.ReadLine());
             int size = 2 * (radius + 1);
 
-            // ---------- TODO ----------
+            for (int i = 0; i <= 2 * radius; i++)
+            {
+                if (i == 0 || i == 2 * radius)
+                {
+                    Console.Write(" ");
+                    for (int j = 1; j <= 2 * radius; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.Write(" ");
+                    for (int c = 0; c <= 2 * radius; c++)
+                    {
+                        if (c == 2 * radius / 3 || c == 4 * radius / 3)
+                        {
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    Console.WriteLine("");
 
-            // --------------------
+                }
+                else if (i == 2 * radius / 3 || i == 4 * radius / 3)
+                {
+                    Console.Write("*");
+                    for (int a = 1; a <= size; a++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int b = 1; b < 2 * radius; b++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine("");
+                }
+                else
+                {
+                    Console.Write("*");
+                    for (int d = 1; d < 2 * radius; d++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write("  ");
+                    for (int c = 0; c <= 2 * radius; c++)
+                    {
+                        if (c == 2 * radius / 3 || c == 4 * radius / 3)
+                        {
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    Console.WriteLine("");
+                }
+            }
         }
 
         // calculate the distance between (x1, y1) and (x2, y2)
